1bd43f8f702710c5762d97ca7de113871f329f4e
Ziwen Wang <collinziwen.wang@mail.utoronto.ca>
2021-03-22 21:04:20 +0000

    adding comments and basic testcases

--- contracts/dGame.sol
+++ contracts/dGame.sol
@@ -12,35 +12,34 @@ contract dGame{
         nftoken = _nftoken;
     }
 
+    // mint Currency Token
     function mintCToken(address account, uint256 amount) public {
         ctoken.mint(account,amount);
     }
 
-    function mintNFToken(address account, string memory _tokenURI) public {
+    // mintNFToken, called in buyNFToken function
+    function mintNFToken(address account, string memory _tokenURI) private {
         nftoken.mint(account, _tokenURI);
     }
 
+    // return account's currency token balance
     function showCurrencyTokenBalance(address account) public view returns (uint256){
         uint256  ctokenBalanceWei = ctoken.balanceOf(account);
         return ctokenBalanceWei;
     }
 
-    function showNFToken(address account) public view returns (string memory){
-        string memory tokenURIs;
-        uint256 balanceOf = nftoken.balanceOf(account);
-        for (uint i = 0; i < balanceOf; i++){
-            uint256 id = nftoken.tokenOfOwnerByIndex(account, i);
-            string memory tokenURI = nftoken.tokenURI(id);
-            tokenURIs = tokenURI;
-        }
-        return tokenURIs;
-    }
-    function showMsgSender() payable public  returns ( address){
+//    function showNFToken(address account) public view returns (string memory){
+//        string memory tokenURIs;
+//        uint256 balanceOf = nftoken.balanceOf(account);
+//        for (uint i = 0; i < balanceOf; i++){
+//            uint256 id = nftoken.tokenOfOwnerByIndex(account, i);
+//            string memory tokenURI = nftoken.tokenURI(id);
+//            tokenURIs = tokenURI;
+//        }
+//        return tokenURIs;
+//    }
 
-        address result = msg.sender;
-        return result;
-    }
-
+    // buy NF token using currency token with input sender address, NF token string and price of token
     function buyNFToken(address account, string memory _tokenURI, uint256 price) payable public {
         uint256 balance = showCurrencyTokenBalance(account);
         require (balance >= price, 'Error, not enough currency tokens');
